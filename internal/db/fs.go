954c4f2d2d3b81ddb68495828ef17bff74b19cdd
Mshivam2409 <shivammalhotra08@gmail.com>
2021-08-14 22:26:57 +0530

    Add Cron Job to limit storage leak

--- internal/db/fs.go
+++ internal/db/fs.go
@@ -30,6 +30,20 @@ func RemoveContents(dir string) error {
 	return nil
 }
 
+func DirSize(path string) (int64, error) {
+	var size int64
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			size += info.Size()
+		}
+		return err
+	})
+	return size, err
+}
+
 func WriteWAV(qid string) (string, error) {
 	b, err := GetWAV(qid)
 
@@ -38,7 +52,6 @@ func WriteWAV(qid string) (string, error) {
 		return "", err
 	}
 
-	log.Println(len(b))
 	fname := uuid.NewV4().String()
 	err = ioutil.WriteFile(fmt.Sprintf("%s/%s.wav", viper.GetString("cache.tempdir"), fname), b, 0777)
 	if err != nil {
@@ -56,7 +69,6 @@ func WriteVTT(qid string) (string, error) {
 		return "", err
 	}
 
-	log.Println(len(b))
 	fname := uuid.NewV4().String()
 	err = ioutil.WriteFile(fmt.Sprintf("%s/%s.vtt", viper.GetString("cache.tempdir"), fname), b, 0777)
 	if err != nil {
